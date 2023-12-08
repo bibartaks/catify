8242502b9e5fffaac6e3b54b65085c93d1e60036
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-12-08 19:17:21 +0600

    Refined landing section: Implemented SASS for improved styling and updated body background color.

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -2,32 +2,40 @@ import { montserrat } from "@/app/fonts"
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
+import styles from "./landing.module.scss"
 
 export default function Landing() {
   return (
-    <header className={`h-[calc(100vh-100px)]`}>
-      <div className="w-[400px] h-[400px] bg-[#7B66FF] opacity-80 mt-20  blur-[8rem] rounded-full z-[-1] absolute"></div>
-
-      <div className="h-[100%] flex  justify-between  items-center  max-w-[1300px] mx-auto">
+    <header className={`h-[calc(100vh-200px)]`}>
+      <div className={styles.gradientBall}></div>
+      <div className="h-[100%] flex flex-col  justify-center  items-center  max-w-[1050px] mx-auto">
+        <h1
+          className={`text-5xl   font-bold ${montserrat.className} text-center leading-[120%] mb-5 }`}
+        >
+          Instantly Embrace the Adoption of a Loving
+          <span className={styles.catTitle}> cat</span> into Your Life
+        </h1>
+        <p
+          className="max-w-[650px] mx-auto text-center mb-8
+         leading-[200%]"
+        >
+          With our intuitive and powerfull platform you can easily adopt your
+          cat without any hassle and find various types of cats with just a
+          second
+        </p>
         <div>
-          <h1 className={`text-5xl ${montserrat.className} font-bold mb-5`}>
-            Adopt Cat In A Second!
-          </h1>
-          <p className="max-w-[500px] text-justify leading-[180%] mb-5">
-            Experience instant feline companionship on our revolutionary
-            platform. Adopt a cat in seconds, skipping the wait and
-            complexities. Redefine adoption with swift and gratifying
-            connections. Join us in creating lasting bonds with just a click.
-          </p>
           <Link
             href="#"
-            className="bg-[#1640D6] px-4 py-2 text-white rounded-full text-sm hover:opacity-80"
+            className="bg-gradient-to-r from-[#4b97f3] to-[blue] px-5 py-3 rounded-full text-white   mr-5 hover:opacity-80"
           >
-            Adopt Now
+            Get Started
           </Link>
-        </div>
-        <div className="z-20">
-          <Image src="/cat.png" width={500} height={500} alt="cat image" />
+          <Link
+            href="#"
+            className="bg-[white] border  border-gray-800 px-5 py-3 rounded-full text-black "
+          >
+            Demo
+          </Link>
         </div>
       </div>
     </header>
