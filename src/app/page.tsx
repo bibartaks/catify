94563e5d06ec620a625871d2275fa623fe91d5e7
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-12-10 22:57:47 +0600

    Added Key Capabilities section and change the app bg gradient position.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import IntroVideo from "@/components/IntroVideo/IntroVideo"
 import KeyCapabilities from "@/components/KeyCapabilities/KeyCapabilities"
 import Landing from "@/components/Landing/Landing"
 import Image from "next/image"
+import styles from "./global.module.scss"
 
 export default function Home() {
   return (
     <>
       <Landing />
-      <IntroVideo />
-      <KeyCapabilities />
+      <div className={styles.videoBg}>
+        <IntroVideo />
+        <KeyCapabilities />
+      </div>
     </>
   )
 }
--- src/components/KeyCapabilities/KeyCapabilities.tsx
+++ src/components/KeyCapabilities/KeyCapabilities.tsx
@@ -14,51 +14,75 @@ export default function KeyCapabilities() {
           Key <span className={styles.spWord}>Capabilities</span>
         </h1>
         {/* Capabilities Container */}
-        <div className="grid grid-cols-3 gap-x-10 py-5">
-          <div className="border pb-5 ">
+        <div className="grid grid-cols-4 gap-x-10 py-5">
+          <div className="border rounded-xl border-black  bg-blue-100 pb-5 ">
             <Image
               src="/natural-language.png"
               width={300}
-              height={200}
+              height={150}
               alt="lol"
-              className="w-[100%]"
+              className="w-[100%] h-[auto] block mx-auto"
             />
-            <h1 className="text-[1.3rem] font-semibold mb-0 px-2 pt-1">
+            <h1
+              className={` ${montserrat.className} text-[1.3rem] font-semibold mb-0 px-2 pt-3`}
+            >
               Adoption cat in a second
             </h1>
-            <p className="px-2 pt-2 leading-[170%]">
+            <p className="px-2 pt-2 leading-[170%] text-sm">
               Communicate with Luna in everyday language. It's like talking to a
               friend who understands you completely.
             </p>
           </div>
-          <div className="border pb-5 ">
+          <div className="border rounded-xl border-black  bg-blue-100 pb-5 ">
             <Image
               src="/natural-language.png"
               width={300}
-              height={200}
+              height={150}
               alt="lol"
-              className="w-[100%]"
+              className="w-[100%] h-[auto] block mx-auto"
             />
-            <h1 className="text-[1.3rem] font-semibold mb-0 px-2 pt-1">
+            <h1
+              className={` ${montserrat.className} text-[1.3rem] font-semibold mb-0 px-2 pt-3`}
+            >
               Adoption cat in a second
             </h1>
-            <p className="px-2 pt-2 leading-[170%]">
+            <p className="px-2 pt-2 leading-[170%] text-sm">
               Communicate with Luna in everyday language. It's like talking to a
               friend who understands you completely.
             </p>
           </div>
-          <div className="border pb-5 ">
+          <div className="border rounded-xl border-black  bg-blue-100 pb-5 ">
             <Image
               src="/natural-language.png"
               width={300}
-              height={200}
+              height={150}
               alt="lol"
-              className="w-[100%]"
+              className="w-[100%] h-[auto] block mx-auto"
             />
-            <h1 className="text-[1.3rem] font-semibold mb-0 px-2 pt-1">
+            <h1
+              className={` ${montserrat.className} text-[1.3rem] font-semibold mb-0 px-2 pt-3`}
+            >
               Adoption cat in a second
             </h1>
-            <p className="px-2 pt-2 leading-[170%]">
+            <p className="px-2 pt-2 leading-[170%] text-sm">
+              Communicate with Luna in everyday language. It's like talking to a
+              friend who understands you completely.
+            </p>
+          </div>
+          <div className="border rounded-xl border-black  bg-blue-100 pb-5 ">
+            <Image
+              src="/natural-language.png"
+              width={300}
+              height={150}
+              alt="lol"
+              className="w-[100%] h-[auto] block mx-auto"
+            />
+            <h1
+              className={` ${montserrat.className} text-[1.3rem] font-semibold mb-0 px-2 pt-3`}
+            >
+              Adoption cat in a second
+            </h1>
+            <p className="px-2 pt-2 leading-[170%] text-sm">
               Communicate with Luna in everyday language. It's like talking to a
               friend who understands you completely.
             </p>
