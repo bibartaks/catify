c6e86cd0cf72e44159df2c06a76a82730f627156
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-12-12 16:08:08 +0600

    Implemented authentication system.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,20 @@ import KeyCapabilities from "@/components/KeyCapabilities/KeyCapabilities"
 import Landing from "@/components/Landing/Landing"
 import Image from "next/image"
 import styles from "./global.module.scss"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session) {
+    redirect("/unauthenticated")
+  }
+
   return (
     <>
       <Landing />
